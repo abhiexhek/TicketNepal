a5fb1a10cc77bf7badb228f4d78b75debab7d99c
abhiexhek <abiseksbd@gmail.com>
2025-07-24 16:38:05 +0545

    Fix frontend

--- Frontend Ticket Nepal/TicketNepal/src/app/auth/login/page.tsx
+++ Frontend Ticket Nepal/TicketNepal/src/app/auth/login/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
+
 import { useState, useContext } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
