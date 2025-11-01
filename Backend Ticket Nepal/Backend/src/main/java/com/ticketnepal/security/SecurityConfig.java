4dda5dd125b16967aa336f72169f4b2e974e82ab
Abhishek Subedi <108131153+abhiexhek@users.noreply.github.com>
2025-11-01 19:55:40 +0545

    Update SecurityConfig.java

--- Backend Ticket Nepal/Backend/src/main/java/com/ticketnepal/security/SecurityConfig.java
+++ Backend Ticket Nepal/Backend/src/main/java/com/ticketnepal/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000", // Next.js default
                 "http://localhost:9002",  // your previous config (keep if needed)
-                "https://ticketnepal-frontend.onrender.com" // Added Render frontend URL
+                "hhttps://ticketnepal-80fs.onrender.com/" // Added Render frontend URL
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
